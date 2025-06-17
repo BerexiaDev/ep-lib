6ae3abb68b99ff0c1cacd21ac5c9170afd951420
Abdelmalek123-Ennani <ahmam3600@gmail.com>
2025-06-17 13:25:32 +0100

    Add BlacklistToken model and JWT encoding/decoding methods with blacklist check

---
+++ ep_lib/jwt_base.py
@@ -0,0 +1,89 @@
+import datetime
+import inject
+import jwt
+import os
+from ep_lib.auth.models.black_list_tokem import BlacklistToken
+from flask_bcrypt import Bcrypt
+
+class JWTbase:
+    
+    @property
+    def password(self):
+        raise AttributeError('password: write-only field')
+
+    @password.setter
+    def password(self, password):
+        flask_bcrypt = inject.instance(Bcrypt)
+        self.password_hash = flask_bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        flask_bcrypt = inject.instance(Bcrypt)
+        return flask_bcrypt.check_password_hash(self.password_hash, password)
+
+    @staticmethod
+    def encode_auth_token(user_id, days= 1, seconds=5, minutes= 0):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            key = os.getenv("SECRET_KEY")
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days= days, seconds=seconds, minutes=minutes),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                key,
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+        
+    @staticmethod
+    def encode_refresh_token(user_id, days=30):
+        """
+        Generates the Refresh Token
+        :return: string
+        """
+        try:
+            key = os.getenv("SECRET_KEY")
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=days),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                key,
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return str(e)
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            key = os.getenv("SECRET_KEY")
+            payload = jwt.decode(auth_token, key, algorithms=['HS256'])
+            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+            if is_blacklisted_token:
+                return {
+                     "status": "fail",
+                     "message": "Token blacklisted. Please log in again.",
+                    } 
+            else:
+                return {"status": "success", "token": payload["sub"]}
+        except jwt.ExpiredSignatureError:
+            return {
+                "status": "fail",
+                "message": "Signature expired. Please log in again.",
+            }
+        except jwt.InvalidTokenError:
+            return {"status": "fail", "message": "Invalid token. Please log in again."}
