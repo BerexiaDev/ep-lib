aa1a5913cb3ff1831ee895f0c3c93bd38983aa2b
Abdelmalek123-Ennani <ahmam3600@gmail.com>
2025-06-17 13:25:46 +0100

    Add Investor and User models with basic attributes and representation methods

---
+++ ep_lib/auth/models/investor.py
@@ -0,0 +1,26 @@
+from ep_lib.document import Document
+from ep_lib.jwt_base import JWTbase
+
+class Investor(Document, JWTbase):
+    __TABLE__ = "investors"
+    
+    email = None
+    password_hash = None
+    full_name = None
+    projet = None
+    property_or_activity_type = None
+    target_regions = None
+    investment_amount = None
+    company_name= None
+    headquarters_location = None
+    profile = None
+    business_sector = None
+    role = None
+    phone = None
+    terms_accepted = None
+    created_on = None
+    updated_on = None
+    
+    
+    def __repr__(self):
+        return "<Investor '{} {}'>".format(self.full_name,self.email)
---
+++ ep_lib/auth/models/user.py
@@ -0,0 +1,22 @@
+from ep_lib.document import Document
+from ep_lib.jwt_base import JWTbase
+
+
+class User(Document, JWTbase):
+    __TABLE__ = "users"
+
+    email = None
+    password_hash = None
+    full_name = None
+    created_on = None
+    modified_on = None
+    admin = None
+    role = None
+    is_active= None
+    is_new_user = None
+    
+
+    def __repr__(self):
+        return "<User '{} {}'>".format(self.first_name,self.last_name)
+
+
