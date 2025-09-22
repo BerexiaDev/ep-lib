73f5135a186743b3daed2b2c9423fd740d348572
Kamal Ourajdal <kamalourajdal@berexia.com>
2025-09-22 17:34:17 +0100

    Implement function to get logged in portal user deatails

--- ep_lib/auth/auth_helper.py
+++ ep_lib/auth/auth_helper.py
@@ -1,4 +1,5 @@
 from ep_lib.auth.user import User
+from ep_lib.auth.portal_user import PortalUser
 
 class AuthHelper:
     
@@ -55,3 +56,38 @@ class AuthHelper:
                 }
             }
             return response_object
+        
+    @staticmethod
+    def get_logged_in_portal_user(request):
+        authorization = request.headers.get('Authorization')
+        auth_token = authorization.split(" ")[1]
+        if auth_token:
+            resp = PortalUser.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                user = PortalUser().load({'_id':resp['token']})
+                response_object = {
+                    'status': 'success',
+                    'data': {
+                        'id': user.id,
+                        'email': user.email,
+                        'full_name': user.full_name,
+                        'is_active': user.is_active,
+                        'references': user.references,
+                        'process': user.process,
+                        'created_on': str(user.created_on),
+                        'role': user.role
+                    }
+                }
+                return response_object, 200
+            response_object = {
+                'status': 'fail',
+                'message': resp
+            }
+            return response_object, 401
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return response_object, 401
+    
