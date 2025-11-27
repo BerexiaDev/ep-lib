95c175ebf2d284b1413a2a37d4f1bb3961808486
Karim Bourass <bourasskarim9@gmail.com>
2025-11-28 00:35:51 +0100

    Fix import path for token_required decorator in audit_controller.py

--- ep_lib/audit_logger/controller/audit_controller.py
+++ ep_lib/audit_logger/controller/audit_controller.py
@@ -5,7 +5,7 @@ from flask import request
 from ep_lib.audit_logger.service.audit_service import get_audit_logs_paginated
 from ep_lib.dto import AuditDto
 from ep_lib.reqparse import get_default_paginated_request_parse
-from ep_lib.auth.decorators import token_required
+from ep_lib.auth.decorator import token_required
 
 api = AuditDto.api
 audit_pagination = AuditDto.audit_pagination
