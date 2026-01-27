26b5b722550522501dbfcfe0808f055096307d16
Abdelmalek123-Ennani <ahmam3600@gmail.com>
2026-01-27 12:25:28 +0100

    feat: Add JWT authentication and authorization decorator with role, screen, and mode-based access control.

--- ep_lib/auth/decorator.py
+++ ep_lib/auth/decorator.py
@@ -83,6 +83,10 @@ def token_required(
                 if not token:
                     return {"message": "Invalid token payload"}, 401
 
+                
+                if not token.get("is_active"):
+                    return {"message": "User is inactive"}, 401
+
                 # Get user role array
                 user_roles = token.get("role", [])
                 
